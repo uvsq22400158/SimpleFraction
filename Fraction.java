24a361b57ca20b8c6778f0c9c079eb3c1b74b717
uvsq22400158 <yanni.doumbia@ens.uvsq.fr>
2024-09-20 23:31:48 +0200

    methode toString

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,9 @@
 public class Fraction{
     public static void main ( String [] args ){
-        System.out.println("Fraction");
+        Fraction f= new Fraction();
+        System.out.println(f.toString());
+    }
+    public String toString(){
+        return "je suis une fraction";
     }
 }
